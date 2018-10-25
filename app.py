5583817eb6d9b4d588d3ac3e6a88629e4244b940
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:54:24 +0300

    dshls

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+	return render_template ("home.html")
+
+@app.route('/shop')
+def shop():
+	products = ["shirt" , " flag" , "poster", "card for game"] 
+	return render_template ("shop.html", products=products)
 
 if __name__ == '__main__':
    app.run(debug = True)
